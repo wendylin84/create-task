c35290278f9568fa27489c3786209de544dbbff6
wendylin84 <wendyl84@nycstudents.net>
2026-01-15 14:02:19 -0500

    e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const characters = [];
+const cards = [
+  {
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+  },
+];
+let dontKnow = {};
+let knowArray = {};
+
+function insert() {
+  const flashcardsContainer = document.querySelector("#flashcards");
+  cards.forEach((card) => {
+    cardElement.insertAdjacentHTML = `
+            <div class="question">${card.question}</div>
+            <div class="answer">${card.answer}</div>
+        `;
+  });
+}
+
+function categorizeKnowledge(card) {
+  const idk = document.querySelector("#dont-know");
+  if (card.answer) {
+    knowArray[card.question] = card.answer;
+  } else {
+    dontKnow[card.question] = true;
+  }
+}
+
+cards.forEach(categorizeKnowledge);
