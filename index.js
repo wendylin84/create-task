6571d814eb0626da389bec03f5360e37230cdd0a
wendylin84 <wendyl84@nycstudents.net>
2026-01-16 14:02:07 -0500

    d

--- index.js
+++ index.js
@@ -8,26 +8,70 @@ const cards = [
     answer: "Jupiter",
   },
 ];
-let dontKnow = {};
+let dontknowArray = {};
 let knowArray = {};
 
-function insert() {
-  const flashcardsContainer = document.querySelector("#flashcards");
-  cards.forEach((card) => {
-    cardElement.insertAdjacentHTML = `
-            <div class="question">${card.question}</div>
-            <div class="answer">${card.answer}</div>
-        `;
+function inject(card) {
+  const flashcards = document.querySelector("#flashcards");
+  flashcards.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+      <h2 class="question">${card.question}</h2>
+    </div>`
+  );
+}
+
+function addtoArray() {
+  const selectCard = document.querySelector(".card");
+  selectCard.addEventListener("click", function (event) {
+    document.innerHTML = "";
   });
 }
 
-function categorizeKnowledge(card) {
-  const idk = document.querySelector("#dont-know");
-  if (card.answer) {
-    knowArray[card.question] = card.answer;
-  } else {
-    dontKnow[card.question] = true;
-  }
+function filterByCategory(category) {
+  let display = document.querySelector("#flashcards");
+  display.innerHTML = "";
+  const filteredCategory = cards.filter((card) => card.category === category);
+  filteredCategory.forEach((card) =>
+    display.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="card">
+      <h2 class="question">${card.question}</h2>
+    </div>`
+    )
+  );
 }
 
-cards.forEach(categorizeKnowledge);
+function filterByButton() {
+  const buttons = document.querySelectorAll("button");
+  const btnArr = Array.from(buttons);
+  btnArr.forEach((btn) =>
+    btn.addEventListener("click", function (event) {
+      if (btn.id === "dontKnow") {
+        filterByCategory("Still Need to Study");
+      }
+      if (btn.id === "know") {
+        filterByCategory("Studied");
+      }
+      if (btn.id === "restart") {
+        let display = document.querySelector("#flashcards");
+        display.innerHTML = "";
+        inject(card);
+      }
+    })
+  );
+}
+filterByButton();
+
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault(); // stops page from refreshing
+  let album = {};
+  album.name = document.getElementById("name").value;
+  album.img = document.getElementById("img").value;
+  album.category = document.getElementById("category").value;
+  album.year = document.getElementById("year").value;
+  console.log(album);
+  songs.push(album);
+  inject(album); // add to the page
+  clearFields(); // reset form inputs
+});
